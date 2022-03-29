00022f722a6d46ca34541ae4e21bbc3aa0a1d5ef
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2022-03-29 20:34:00 +1100

    removed null: false from username column

--- src/find-a-table/db/migrate/20220329071837_add_username_to_user.rb
+++ src/find-a-table/db/migrate/20220329071837_add_username_to_user.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :username, :string, index: true, null: false
-    add_index :users, :username, unique: true, null: false
+    add_column :users, :username, :string, index: true
+    add_index :users, :username, unique: true
   end
 end
